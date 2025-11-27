db1a2c79f090366ffab591885c237b67f1c67a42
Harisankar <harisankar@uniqnex360.com>
2025-11-27 15:45:28 +0530

    401:Made to login,redirected

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Login from './components/Login';
-import AdminHomePage from './components/adminFlow/HomePage'; 
-import SuperAdminHomePage from './components/superAdminFlow/SuperAdminHomePage'; 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Login from "./components/Login";
+import AdminHomePage from "./components/adminFlow/HomePage";
+import SuperAdminHomePage from "./components/superAdminFlow/SuperAdminHomePage";
+import ProtectedRoute from "./utils/ProtectedRoute.js";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="" element={<Login />} />
-                <Route path="*" element={<Navigate to="" />} />
-        <Route path="/Admin/*" element={<AdminHomePage />} />
-        <Route path="/SuperAdmin/*" element={<SuperAdminHomePage />} /> 
+        <Route path="*" element={<Navigate to="" />} />
+        <Route path="/Admin/*" element={<ProtectedRoute><AdminHomePage /></ProtectedRoute>} />
+        <Route path="/SuperAdmin/*" element={<ProtectedRoute><SuperAdminHomePage /></ProtectedRoute>} />
       </Routes>
     </Router>
   );
